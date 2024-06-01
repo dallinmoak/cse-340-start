39b063657081782e05a3fe478ce74daa98dc41d1
dallinmoak <dallinmoak@gmail.com>
2024-06-01 10:40:39 -0600

    basic item route

--- routes/inventory.js
+++ routes/inventory.js
@@ -7,5 +7,17 @@ router.get(
   "/type/:classificationId",
   inventoryController.buildByClassificationId
 );
+router.get("/item/:itemId", (req, res, next) => {
+  res.render("pages/inventory/item", {
+    title: "Item Detail",
+    navData: [],
+    invItem: {
+      id: 1,
+      name: "Item Name",
+      description: "Item Description",
+      price: 100.0,
+    },
+  });
+});
 
 export default router;

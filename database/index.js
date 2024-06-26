bef6587197ad4a8d05caaa83c55b1de721553294
dallinmoak <dallinmoak@gmail.com>
2024-06-26 15:55:36 -0600

    allow dev to opt out of query logging

--- database/index.js
+++ database/index.js
@@ -3,11 +3,17 @@ import "dotenv/config";
 
 const Pool = pg.Pool;
 
+const devMode = process.env.NODE_ENV === "development";
+//log queries by default in dev mode
+let shouldLogQueries = devMode;
+//override log queries setting from env variable
+if (process.env.DEV_PREFS_LOG_QUERY === "false") shouldLogQueries = false;
+
 let defaultExport;
 const dbConnection = {
   connectionString: process.env.DB_CONNECTION_STRING,
 };
-if (process.env.NODE_ENV === "development") {
+if (devMode) {
   const pool = new Pool({
     ...dbConnection,
     ssl: { rejectUnauthorized: false },
@@ -16,7 +22,9 @@ if (process.env.NODE_ENV === "development") {
     query: async (text, params) => {
       try {
         const res = await pool.query(text, params);
-        console.log("query executed successfully", { text });
+        if (shouldLogQueries) {
+          console.log("query executed successfully", { text });
+        }
         return res;
       } catch (e) {
         console.error(e, { text });

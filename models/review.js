e74278ef6bc50f18358b6c217297ba9f4b1b93ac
dallinmoak <dallinmoak@gmail.com>
2024-07-18 07:25:43 -0600

    call pg query properly

--- models/review.js
+++ models/review.js
@@ -1,10 +1,9 @@
 import pool from "../database/index.js";
-const { query } = pool;
 
 const createReview = async (id, authorId, review) => {
   try {
     const queryText = `insert into course_340.review (inv_id, author_id, text) values ($1, $2, $3) returning *`;
-    const res = await query(queryText, [id, authorId, review]);
+    const res = await pool.query(queryText, [id, authorId, review]);
     return res.rows[0];
   } catch (e) {
     throw new Error(e.message);
@@ -14,25 +13,30 @@ const createReview = async (id, authorId, review) => {
 const getReviewsByAuthor = async (authorId) => {
   try {
     const queryText = `select r.review_id as id, r.text, r.date, i.inv_id as inv_id, i.inv_make, i.inv_model, i.inv_year, a.account_id as author_id, a.account_firstname as author_firstName, a.account_lastname as author_lastName from course_340.review as r join course_340.inventory as i on r.inv_id = i.inv_id join course_340.account as a on r.author_id = a.account_id where r.author_id = $1`;
-    const res = await query(queryText, [authorId]);
-    return res.rows.map((row) => {
-      return {
-        id: row.id,
-        text: row.text,
-        date: row.date,
-        item: {
-          id: row.inv_id,
-          make: row.inv_make,
-          model: row.inv_model,
-          year: row.inv_year,
-        },
-        author: {
-          id: row.author_id,
-          firstName: row.author_firstname,
-          lastName: row.author_lastname,
-        },
-      };
-    });
+    const res = await pool.query(queryText, [authorId]);
+    if (res) {
+      console.log("res", res);
+      return res.rows.map((row) => {
+        return {
+          id: row.id,
+          text: row.text,
+          date: row.date,
+          item: {
+            id: row.inv_id,
+            make: row.inv_make,
+            model: row.inv_model,
+            year: row.inv_year,
+          },
+          author: {
+            id: row.author_id,
+            firstName: row.author_firstname,
+            lastName: row.author_lastname,
+          },
+        };
+      });
+    } else {
+      throw new Error("res was falsy");
+    }
   } catch (e) {
     throw new Error(e.message);
   }
@@ -41,7 +45,7 @@ const getReviewsByAuthor = async (authorId) => {
 const getReviewById = async (reviewId) => {
   try {
     const queryText = `select r.review_id as id, r.text, r.date, i.inv_id as inv_id, i.inv_make, i.inv_model, i.inv_year, a.account_id as author_id, a.account_firstname as author_firstName, a.account_lastname as author_lastName from course_340.review as r join course_340.inventory as i on r.inv_id = i.inv_id join course_340.account as a on r.author_id = a.account_id where r.review_id = $1`;
-    const res = await query(queryText, [reviewId]);
+    const res = await pool.query(queryText, [reviewId]);
     return {
       id: res.rows[0].id,
       text: res.rows[0].text,
